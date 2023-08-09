bb3f65caf384fa639fe90dd4827ef607c471ce38
Utsab Luitel <utsab.luitel@ebpearls.com>
2023-08-09 09:35:58 +0545

    add promise

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -10,7 +10,7 @@ import { UserType } from './users.type';
 export class UsersService {
   constructor(@InjectModel(USER_MODEL) private readonly userModel: Model<UserDocument>) {}
 
-  create(createUser: CreateUserDto) {
+  create(createUser: CreateUserDto): Promise<UserType> {
     return this.userModel.create(createUser);
   }
 
@@ -18,7 +18,7 @@ export class UsersService {
     return await this.userModel.find();
   }
 
-  async getById(id: string) {
+  async getById(id: string): Promise<UserType> {
     return await this.userModel.findOne({ _id: id });
   }
 
